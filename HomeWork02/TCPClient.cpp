f20d2e37cdee6210f257f872a4981625caa0b8cb
Thu Hien <thuhien.bk0301@gmail.com>
2022-05-07 11:25:23 +0700

    HomeWork02

---
+++ HomeWork02/TCPClient.cpp
@@ -0,0 +1,77 @@
+﻿//BTVN ngay 08/5/2022: TCPClient, kết nối đến một máy chủ xác
+// định bởi tên miền hoặc địa chỉ IP.Sau đó nhận dữ liệu từ bàn
+// phím và gửi đến server.Tham số được truyền vào từ dòng
+// lệnh có dạng TCPClient.exe <Địa chỉ IP / Tên miền> <Cổng>
+
+
+#include <stdio.h> //chuyen sang ngon ngu C de co the vao ra duoc cac ham cua C
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <cstdio>
+
+
+#define BUFSIZE 1000
+
+#pragma comment(lib,"ws2_32")
+#pragma warning(disable:4996)
+
+int main(int argc, char* argv[])
+{
+    WSADATA wsaData;
+    WORD wVersion = MAKEWORD(2, 2);
+    if (WSAStartup(wVersion, &wsaData)) {
+        printf("Version not supported");
+    }
+
+    SOCKET client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(argv[1]); //localhost/IP
+    addr.sin_port = htons(atoi(argv[2])); //tuong tu nhu ben server
+
+    int ret = connect(client, (SOCKADDR*)&addr, sizeof(addr));
+    if (ret == SOCKET_ERROR)
+    {
+        ret = WSAGetLastError();
+        printf("Connect failed: %d\n", ret);
+        return 0;
+    }
+
+    // nhan xau chao tu Server
+    char buf[256];
+    int result = recv(client, buf, sizeof(buf), 0);
+
+    //neu ket noi bi ngat
+    if (result <= 0)
+    {
+        printf("Connect closed!\n");
+        return 1;
+    }
+
+    //neu co du lieu thi them ki tu ket thuc xau
+    if (result < sizeof(buf))
+   
+        buf[result] = 0;
+        // in cau chao ra man hinh
+    printf("From server: %s\n", buf);
+
+    //doc du lieu tu ban phim, gui len server
+    while (1)
+    {
+        printf("Nhap du lieu:");
+        gets_s(buf, sizeof(buf));
+
+        // gui cho server
+        send(client, buf, strlen(buf), 0);
+        // neu nguoi dung go exit thi thoat
+        if (strcmp(buf, "exit") == 0)
+        {
+            break;
+        }
+    }
+    closesocket(client);
+    WSACleanup();
+
+
+}
---
+++ HomeWork02/TCPServer.cpp
@@ -0,0 +1,91 @@
+﻿//BTVN ngay 8/5: TCPServer, đợi kết nối ở cổng xác định
+// bởi tham số dòng lệnh.Mỗi khi có client kết nối đến, thì gửi
+// xâu chào được chỉ ra trong một tệp tin xác định, sau đó ghi
+// toàn bộ nội dung client gửi đến vào một tệp tin khác được chỉ
+// ra trong tham số dòng lệnh
+
+#include <stdio.h> 
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib,"ws2_32")
+#pragma warning(disable:4996)
+
+
+int main(int argc, char* argv[])
+{
+    // Khoi tao
+    WSADATA wsaData;
+    WORD wVersion = MAKEWORD(2, 2);
+    if (WSAStartup(wVersion, &wsaData)) {
+        printf("Version not supported");
+    }
+
+    //Tao doi tuong Socket
+    SOCKET listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    //Khai bao dia chi server
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(atoi(argv[1]));
+
+    //Gan dia chi vao socket
+    bind(listener, (SOCKADDR*)&addr, sizeof(addr));
+
+    // Chuyen sang trang thai cho ket noi
+    listen(listener, 5);
+
+    printf("Dang cho ket noi moi..\n");
+    SOCKET client = accept(listener, NULL, NULL); // chap nhan ket noi, luu vao client
+
+    char buf[256];
+    int ret;
+
+    // Doc xau chao tu file
+    FILE* f;
+    if ((f = fopen(argv[2], "r")) == NULL) {
+        printf("Error opening text file.\n");
+    }
+
+    fgets(buf, 256, f);
+
+    printf("%s\n", buf);
+
+    fclose(f);
+
+    // Gui sang client
+    send(client, buf, strlen(buf), 0);
+
+    // Nhan du lieu tu client va ghi vao file
+    FILE* f2;
+
+     
+    // TCPServer.exe <Cổng> <Tệp tin chứa câu chào> <Tệp tin lưu nội dung client gửi đến> --> argv[3] = <tên tệp tin lưu ND client gửi đến
+  
+    printf("%s\n", argv[3]);
+
+    while (1)
+    {
+        if ((f2 = fopen(argv[3], "a")) == NULL) {
+            printf("Error opening text file.\n");
+        }
+        char recvBuf[256];
+        ret = recv(client, recvBuf, sizeof(recvBuf), 0);
+        if (ret <= 0) {
+            return -1;
+        }
+        recvBuf[ret] = '\0';
+        printf("%s\n", recvBuf);
+       
+        fprintf(f2, "%s\n", recvBuf);
+        fclose(f2);
+    }
+
+
+
+    closesocket(client);
+    closesocket(listener);
+    WSACleanup();
+
+}
